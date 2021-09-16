5f565289f262f87de057bbeb04fa36c15aac9640
apozel <tomchaillan@gmail.com>
2021-09-16 18:56:11 +0000

    change for create file from docker-compose

--- install.sh
+++ install.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-#liste des medias possible :
-listePlex=("Movies" "Shows" "Music" "AudioBook" "transcode")
 
-#liste des containers :
-listeContainer=("plex" "jackett" "transmission" "nzbget" "sonarr" "radarr" "overseerr" "letsencrypt" "timemachine" "portainer" "watchtower")
 
-#special folder
-listeFolder=("nzbget/downloads" "transmission/download" "transmission/blackhole")
-
-# add config folder
-
-for x in $(ls ./config); do
-  mkdir "config/${x}/config"
-done
-
-#add media folder
-for title in $listePlex;
-do
-mkdir "config/plex/${title}"
-done
-
-# add special folder
-for folder in $listeFolder;
-do
-mkdir "config/${folder}"
+for x in $(awk '$1 == "-"{ if (key == "volumes:") print $NF; next } {key=$1}' docker-compose.yml | cut  -d ':' -f 1); do
+  mkdir "${x}"
 done
