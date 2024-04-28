c13e0bf67f82d9fbcfacea9ddebc8f443e8bb2e1
apozel <tomchaillan@gmail.com>
2024-04-28 11:24:53 +0200

    mise a jours sur omen

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 
 
-for x in $(awk '$1 == "-"{ if (key == "volumes:") print $NF; next } {key=$1}' docker-compose.yml | cut  -d ':' -f 1); do
-  mkdir "${x}"
+for x in $(awk '$1 == "-"{ if (key == "volumes:") print $NF; next } {key=$1}' docker-compose.yml | cut  -d ':' -f 1 | grep -v docker); do
+  mkdir -p "${x}"
 done
